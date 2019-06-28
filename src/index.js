7e7b8cb0d1336e119cea95ea19f79fc96152351a
levon <levon.gambaryan@gmail.com>
2019-06-28 23:11:51 +0300

    add fontawesome

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
-// import somescript from './js/somescript';
-import './styles/_stylebase.scss';
-
 import Favicon from './images/favicon.jpg'; // eslint-disable-line no-unused-vars
+
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/solid';
+import '@fortawesome/fontawesome-free/js/regular';
+import '@fortawesome/fontawesome-free/js/brands';
+
+import './styles/_stylebase.scss';
 // import Someimage from './images/layout/someimagе.png';
+// import somescript from './js/somescript';
 
 console.log('Hello!');
 
