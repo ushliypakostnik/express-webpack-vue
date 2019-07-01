140354b2d63f5dbab027971d2933755156b79bb6
levon <levon.gambaryan@gmail.com>
2019-07-01 18:17:27 +0300

    test

--- src/components/app.vue
+++ src/components/app.vue
@@ -1,11 +1,27 @@
 <template>
   <div id="app">
     <h1>Vue Component!!!</h1>
+    <div id="test">{{ name }}</div>
+    <input
+      id="name"
+      v-model="name"
+      type="text"
+      name="name"
+    >
   </div>
 </template>
 
 <script>
-  export default { }
+export default {
+  data() {
+    return {
+      name: null,
+    };
+  },
+  methods: {
+
+  },
+};
 </script>
 
 <style lang="scss">
